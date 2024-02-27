da0eb19f67292c3294341d3c6430454a6b78c836
Dmitriy Polyakov <dmitriy.i.polyakov@rt.ru>
2024-02-27 22:08:49 +0300

    HeroMoving refactoring

--- Assets/Scripts/HeroMoving.cs
+++ Assets/Scripts/HeroMoving.cs
@@ -1,70 +1,87 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class HeroMoving : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
-
-    private static readonly int IsJumping = Animator.StringToHash("isJumping");
-    private static readonly int XVelocity = Animator.StringToHash("xVelocity");
-    private static readonly int YVelocity = Animator.StringToHash("yVelocity");
+    
+    private static readonly int State = Animator.StringToHash("state");
     
     private Rigidbody2D _rigidbody;
     private Animator _animator;
+    private SpriteRenderer _renderer;
     private float _horizontalInput;
-    private bool _isFacingRight;
     private bool _isGrounded;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-        _isFacingRight = true;
+        _renderer = GetComponent<SpriteRenderer>();
         _isGrounded = true;
     }
 
     private void Update()
     {
         _horizontalInput = Input.GetAxisRaw("Horizontal");
-
-        FlipSprite();
         
+        _rigidbody.velocity = new Vector2(_horizontalInput * _speed, _rigidbody.velocity.y);
+
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
-            _isGrounded = false;
-            _animator.SetBool(IsJumping, !_isGrounded);
-        }
-    }
 
-    private void FixedUpdate()
-    {
-        _rigidbody.velocity = new Vector2(_horizontalInput * _speed, _rigidbody.velocity.y);
+            _isGrounded = false;
+        }
         
-        _animator.SetFloat(XVelocity, Math.Abs(_rigidbody.velocity.x));
-        _animator.SetFloat(YVelocity, _rigidbody.velocity.y);
+        UpdateAnimationState();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         _isGrounded = true;
-        _animator.SetBool(IsJumping, !_isGrounded);
     }
-
-    private void FlipSprite()
+    
+    private void UpdateAnimationState()
     {
-        if (_isFacingRight && _horizontalInput < 0f || !_isFacingRight && _horizontalInput > 0f)
+        MovementState state;
+
+        if (_horizontalInput > 0f)
         {
-            _isFacingRight = !_isFacingRight;
-            
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            
-            transform.localScale = localScale;
+            state = MovementState.Running;
+            _renderer.flipX = false;
         }
+        else if (_horizontalInput < 0f)
+        {
+            state = MovementState.Running;
+            _renderer.flipX = true;
+        }
+        else
+        {
+            state = MovementState.Idle;
+        }
+
+        if (_rigidbody.velocity.y > 0f && _isGrounded == false)
+        {
+            state = MovementState.Jumping;
+        }
+        else if (_rigidbody.velocity.y < 0f && _isGrounded == false)
+        {
+            state = MovementState.Falling;
+        }
+
+        _animator.SetInteger(State, (int) state);
+    }
+    
+    private enum MovementState
+    {
+        Idle, 
+        Running, 
+        Jumping, 
+        Falling
     }
 }
 
